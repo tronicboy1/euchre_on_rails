1d217860a181153bc62c79bdbac578fb9d771879
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-08-19 17:08:49 +0900

    added validators to userdata class

--- app/models/userdata.rb
+++ app/models/userdata.rb
@@ -1,3 +1,6 @@
-class Userdata < ActiveRecord::Base
-  self.table_name = "userdata"
+# class Userdata < ActiveRecord::Base
+class Userdata < ApplicationRecord
+  #self.table_name = "userdata"
+  validates :username, presence: true, uniqueness: true
+  validates :password_hash, presence: true
 end
