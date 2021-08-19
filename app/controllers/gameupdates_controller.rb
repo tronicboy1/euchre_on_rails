ff21281575a8a955a1860a423c1e020177e6bd95
Austin Inoue <inoueaus@icloud.com>
2021-08-20 08:38:07 +0900

    added edit features to db test

--- app/controllers/gameupdates_controller.rb
+++ app/controllers/gameupdates_controller.rb
@@ -22,4 +22,19 @@ class GameupdatesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @update_edit = Gameupdates.find(params[:id])
+  end
+
+  def update
+    @new_update = Gameupdates.new
+    @new_update.text = params[:gameupdates][:text]
+    if @new_update.save
+      flash[:primary] = "Update was saved"
+      redirect_to '/gameupdates/'
+    else
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   get '/game', to: 'game#show'
-  resources :gameupdates, only: [:show,:index,:new,:create]
+  resources :gameupdates, only: [:show,:index,:new,:create,:edit,:update]
 end
