176d9398b03926c91a0a3da5b4d0f3c2a4a5ec71
Austin Inoue <inoueaus@icloud.com>
2021-08-20 21:07:47 +0900

    changed naming to update

--- app/controllers/gameupdates_controller.rb
+++ app/controllers/gameupdates_controller.rb
@@ -6,17 +6,17 @@ class GameupdatesController < ApplicationController
   end
 
   def index
-    @result = Gameupdates.last(5)
+    @updates = Gameupdates.last(5)
   end
 
   def new
-    @result = Gameupdates.new
+    @update = Gameupdates.new
   end
 
   def create
-    @result = Gameupdates.new(set_update_params)
+    @update = Gameupdates.new(set_update_params)
     #@update.text = params[:gameupdates][:text]
-    if @result.save
+    if @update.save
       flash[:primary] = "Update was saved"
       redirect_to '/gameupdates/'
     else
@@ -30,8 +30,8 @@ class GameupdatesController < ApplicationController
   end
 
   def update
-    @result.update(set_update_params)
-    if @result.save
+    @update.update(set_update_params)
+    if @update.save
       flash[:primary] = "Update was saved"
       redirect_to '/gameupdates/'
     else
@@ -42,7 +42,7 @@ class GameupdatesController < ApplicationController
 
   def destroy
 
-    @result.destroy
+    @update.destroy
     flash[:success] = "Successfully deleted."
     redirect_to '/gameupdates/'
   end
@@ -53,7 +53,7 @@ class GameupdatesController < ApplicationController
   #private ensures that methods are only used in this rb file
   private
   def set_updateid
-    @result = Gameupdates.find(params[:id])
+    @update = Gameupdates.find(params[:id])
   end
 
   def set_update_params
