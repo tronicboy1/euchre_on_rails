3a300ad6b4d6a4ac1d8cd7505c9a27c62e7b117d
Austin Inoue <inoueaus@icloud.com>
2021-08-21 09:36:02 +0900

    fixed password confirmation and added db save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      byebug
+      flash[:success] = "Account successfully created. Welcome to Euchre on Rails!"
+      redirect_to root_url
+    else
+      params[:action] = "new"
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password,:password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 # class Userdata < ActiveRecord::Base
 class User < ApplicationRecord
   self.table_name = 'user'
+  #attr_accessor :password_confirmation
   before_save { self.username = username.downcase}
-  #self.table_name = "userdata"
+
   validates :username, presence: true, uniqueness: true
-  #validates :password_hash, presence: true
+  validates :password, presence: true, confirmation: true
+  validates_confirmation_of :password
+
   has_many :gameupdates
   has_secure_password
 end
