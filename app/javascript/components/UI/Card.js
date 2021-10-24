949b67318ad690688495071ee37986449342ad20
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-10-24 14:36:30 +0900

    fade modal

--- app/javascript/components/UI/Card.js
+++ app/javascript/components/UI/Card.js
@@ -3,9 +3,16 @@ import React from "react";
 import styles from "./Card.module.css";
 
 const Card = (props) => {
-    return (
-        <div className={`${styles.card} ${styles[props.className]} ${styles[props.className2]}`}>{props.children}</div>
-    );
+  return (
+    <div
+      className={`${styles.card} ${styles[props.className]} ${
+        styles[props.className2]
+      }`}
+      style={props.style}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
