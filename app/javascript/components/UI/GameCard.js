050ac840b83a71b592901ee468252ae5e4831792
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-10-25 11:31:49 +0900

    styling changes

--- app/javascript/components/UI/GameCard.js
+++ app/javascript/components/UI/GameCard.js
@@ -3,18 +3,18 @@ import React from "react";
 import styles from "./GameCard.module.css";
 
 const GameCard = (props) => {
-    const cardNo = props.cardNo;
-    const onCardClick = () => {
-        props.onClick(cardNo);
-    };
+  const cardNo = props.cardNo;
+  const onCardClick = () => {
+    props.onClick(cardNo);
+  };
 
-    const imgData = "data:image/png;base64," + props.b64Img;
+  const imgData = "data:image/png;base64," + props.b64Img;
 
-    return (
-        //<Card className="gamecard">
-            <img className={styles.gamecard} onClick={onCardClick} src={imgData} />
-        //</Card>
-    );
+  return (
+    //<Card className="gamecard">
+    <img className={styles.gamecard} onClick={onCardClick} src={imgData} />
+    //</Card>
+  );
 };
 
 export default GameCard;
