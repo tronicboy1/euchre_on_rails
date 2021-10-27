b4f406a323794d3a83777e8716d298a36b36870c
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-10-27 17:40:39 +0900

    fixed select

--- app/javascript/components/UI/Select.js
+++ app/javascript/components/UI/Select.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Select = (props, ref) => {
-    const Options = props.options.map(option => <option value={option[1]}>{option[0]}</option>)
+    const options = props.options.map(option => <option key={option[1]} value={option[1]}>{option[0]}</option>)
     return (
-        <select ref={ref} ><Options /></select>
+        <select ref={ref} >{options}</select>
     )
 };
 
