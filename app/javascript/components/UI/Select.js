9bf2b48499bd38dc9d6f64d1d557129c97e06f8f
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-10-27 19:34:59 +0900

    add player selection to room creation

--- app/javascript/components/UI/Select.js
+++ app/javascript/components/UI/Select.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import styles from "./Select.module.css";
+
 const Select = (props, ref) => {
-    const options = props.options.map(option => <option key={option[1]} value={option[1]}>{option[0]}</option>)
-    return (
-        <select ref={ref} >{options}</select>
-    )
+  const options = props.options.map((option) => (
+    <option key={option[1]} value={option[1]}>
+      {option[0]}
+    </option>
+  ));
+  return (
+    <div className={styles.box}>
+      <label htmlFor={props.name}>{props.label}</label>
+      <select name={props.name} ref={ref}>
+        {options}
+      </select>
+    </div>
+  );
 };
 
 export default React.forwardRef(Select);
--- app/javascript/pages/CreateRoom/CreateRoom.js
+++ app/javascript/pages/CreateRoom/CreateRoom.js
@@ -1,16 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+
+import styles from "./CreateRoom.module.css";
+
 import Card from "../../components/UI/Card";
 import Select from "../../components/UI/Select";
+import Button from "../../components/UI/Button";
 
 const CreateRoom = () => {
   const username = useSelector((state) => state.auth.username);
-  const users = useSelector(state => state.auth.users);
-  const selectRef = useRef('')
-  console.log(users);
+  const userId = useSelector((state) => state.auth.userId);
+  const users = useSelector((state) => state.auth.users);
+
+  const [formError, setFormError] = useState(false);
+
+  const p2Ref = useRef("");
+  const p3Ref = useRef("");
+  const p4Ref = useRef("");
 
   const submitHandler = (e) => {
-      e.preventDefault();
+    e.preventDefault();
+    const p2IsValid = p2Ref.current.value === "computer" ? true : p2Ref.current.value !==
+    if (isValid) {
+      const playerList = {
+        p1: userId,
+        p2: p2Ref.current.value,
+        p3: p3Ref.current.value,
+        p4: p4Ref.current.value,
+      };
+      console.log(playerList);
+    } else {
+      setFormError(true);
+    }
   };
 
   return (
@@ -23,8 +44,28 @@ const CreateRoom = () => {
       <Card className="form">Game Updates here</Card>
       <Card className="form">
         <Card className="form-inner">
-          <form onSubmit={submitHandler}>
-              <Select ref={selectRef} options={users}></Select>
+          <h3>Start a new game</h3>
+          {formError && <p>Looks like you selected the same user twice!</p>}
+          <form className={styles.form} onSubmit={submitHandler}>
+            <Select
+              name="p2"
+              label="Player 2"
+              ref={p2Ref}
+              options={users}
+            ></Select>
+            <Select
+              name="p3"
+              label="Player 3"
+              ref={p3Ref}
+              options={users}
+            ></Select>
+            <Select
+              name="p4"
+              label="Player 4"
+              ref={p4Ref}
+              options={users}
+            ></Select>
+            <Button type="submit">Create Room</Button>
           </form>
         </Card>
       </Card>
